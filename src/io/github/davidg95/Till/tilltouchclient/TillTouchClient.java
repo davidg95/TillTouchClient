8a17e844aacda88c61bd6613cfdd049f5290b25d
davidg95 <pirakaleader@googlemail.com>
2016-10-31 16:05:42 +0000

    Initial commit.

---
+++ src/io/github/davidg95/Till/tilltouchclient/TillTouchClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.davidg95.Till.tilltouchclient;
+
+/**
+ *
+ * @author 1301480
+ */
+public class TillTouchClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new GUI().setVisible(true);
+    }
+    
+}
